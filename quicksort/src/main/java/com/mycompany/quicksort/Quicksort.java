bc30c68a099dece8cdbad2e0e9a4bd0c885420f5
ElberthMartin <1438878@senati.pe>
2023-08-24 14:44:51 -0500

    divicion la entrada en un array

--- quicksort/src/main/java/com/mycompany/quicksort/Quicksort.java
+++ quicksort/src/main/java/com/mycompany/quicksort/Quicksort.java
@@ -14,5 +14,6 @@ public class Quicksort {
 
     // Solicitamos la  entrada de números separados por comas al usuario
     String input = JOptionPane.showInputDialog("Ingrese los números separados por comas:");
-    
+    // Dividimos la entrada en un array de strings utilizando unas coma como delimitador
+    String[] inputArray = input.split(",");
     }
