dd571a23646d037b98928dd80777a4b67aa319e8
ElberthMartin <1438878@senati.pe>
2023-08-24 14:41:50 -0500

    Solicitamos la  entrada de números separados por comas al usuario

---
+++ quicksort/src/main/java/com/mycompany/quicksort/Quicksort.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quicksort;
+//importar
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Quicksort {
+
+    // Solicitamos la  entrada de números separados por comas al usuario
+    String input = JOptionPane.showInputDialog("Ingrese los números separados por comas:");
+    
+    }
