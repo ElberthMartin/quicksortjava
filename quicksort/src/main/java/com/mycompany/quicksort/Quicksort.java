0713ff5e27ba2da50fd40aa5dd1ca459a78e20dc
ElberthMartin <1438878@senati.pe>
2023-08-24 14:47:45 -0500

    creamos un array de enteros y tambien convertimos los string

--- quicksort/src/main/java/com/mycompany/quicksort/Quicksort.java
+++ quicksort/src/main/java/com/mycompany/quicksort/Quicksort.java
@@ -16,4 +16,11 @@ public class Quicksort {
     String input = JOptionPane.showInputDialog("Ingrese los números separados por comas:");
     // Dividimos la entrada en un array de strings utilizando unas coma como delimitador
     String[] inputArray = input.split(",");
+  // Creamos otro array de enteros para almacenar los números
+    int[] array = new int[inputArray.length];
+
+    // Ahora convertimos los strings a enteros y almacenarlos en el array
+    for (int i = 0; i < inputArray.length; i++) {
+        array[i] = Integer.parseInt(inputArray[i].trim());
+    }
     }
